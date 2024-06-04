3fc8c018f84e873400077e9b34d3db055f7d5b05
dalramjwi <dalramjwi@gmail.com>
2024-06-04 15:23:28 +0900

    반복문 설정 잘 해야함 느낌

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ const data = fs.readFileSync(path, "utf-8", (err, data) => {
 });
 const adata = data.split("\r\n");
 const line = adata[0];
-// console.log(adata);
 const sline = line.split(",");
 // console.log(sline);
+const wdata = adata[1];
+const swdata = wdata.split(",");
+// console.log(swdata);
 function objMake(a, b) {
   let obj = {};
   obj = { [a]: b };
   return obj;
 }
-let test = objMake(sline, "A");
+let test = objMake(sline[0], swdata[0], sline[1], swdata[1]);
+const array = [];
+array.push(test);
 console.log(test);
