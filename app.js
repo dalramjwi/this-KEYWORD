fd205ea10e967dd2227506a83432ea2196282cf7
dalramjwi <dalramjwi@gmail.com>
2024-06-04 15:37:58 +0900

    for each 사용하려 함

--- app.js
+++ app.js
@@ -9,15 +9,29 @@ const adata = data.split("\r\n");
 const line = adata[0];
 const sline = line.split(",");
 // console.log(sline);
-const wdata = adata[1];
-const swdata = wdata.split(",");
-// console.log(swdata);
-function objMake(a, b) {
+adata.forEach((item, index) => {
+  let number = {};
+  number = { [index]: item };
+  // console.log(number);
+  console.log(number["0"]);
   let obj = {};
-  obj = { [a]: b };
-  return obj;
-}
-let test = objMake(sline[0], swdata[0], sline[1], swdata[1]);
-const array = [];
-array.push(test);
-console.log(test);
+  for (let i = 0; i < adata.length; i++) {
+    obj = { [`number['${i}']`]: 1 };
+  }
+  console.log(obj);
+});
+// sline.forEach((item)=>{
+
+// })
+// const wdata = adata[1];
+// const swdata = wdata.split(",");
+// // console.log(swdata);
+// function objMake(a, b) {
+//   let obj = {};
+//   obj = { [a]: b };
+//   return obj;
+// }
+// let test = objMake(sline[0], swdata[0], sline[1], swdata[1]);
+// const array = [];
+// array.push(test);
+// console.log(test);
