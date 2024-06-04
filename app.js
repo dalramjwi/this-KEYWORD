ec1f8a4c9e90fe95c5f81e0af1d7dbe1074bb0b0
dalramjwi <dalramjwi@gmail.com>
2024-06-04 14:41:09 +0900

    readfile 제대로 진행되지 않는 문제

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
-const path = "한국전력공사_지역별 전기차 충전기 현황정보_20221231.csv";
-fs.readFile(path, "utf-8", (err, data) => {
+const path = "utf8.csv";
+const data = fs.readFiles(path, "utf-8", (err, data) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(data);
   }
 });
+console.log(data);
