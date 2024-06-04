d8ee89543878efffd1c8492c60e7c2550752422a
dalramjwi <dalramjwi@gmail.com>
2024-06-04 17:20:24 +0900

    완성본

--- app.js
+++ app.js
@@ -15,20 +15,9 @@ for (let i = 1; i < splitdata.length; i++) {
   let local = {};
   for (let j = 0; j < name.length; j++) {
     let obj = {};
-    let na = name[j];
+    let a = name[j];
     let b = listArr[j];
-    // local[a] = b;
-    // let child = name[j];
-    // let child2 = listArr[j];
-    // function a(a, b) {
-    //   let obj = {};
-    //   obj = { [a]: b };
-    //   return obj;
-    // }
-    function a(a, b) {
-      obj[a] = b;
-      return obj;
-    }
-    console.log(a(na, b));
+    local[a] = b;
+    console.log(local);
   }
 }
