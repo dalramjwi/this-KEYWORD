f254fe36111ae1e48a8bb1285de15a552eee7c2d
dalramjwi <dalramjwi@gmail.com>
2024-06-04 16:28:05 +0900

    try about 이해

--- app.js
+++ app.js
@@ -5,24 +5,45 @@ const data = fs.readFileSync(path, "utf-8", (err, data) => {
     console.error(err);
   }
 });
+
 const adata = data.split("\r\n");
 const line = adata[0];
 const sline = line.split(",");
+//*sline은 만들어질 객체의 key 배열이다.
 // console.log(sline);
-adata.forEach((item, index) => {
-  let number = {};
-  number = { [index]: item };
-  // console.log(number);
-  console.log(number["0"]);
-  let obj = {};
-  for (let i = 0; i < adata.length; i++) {
-    obj = { [`number['${i}']`]: 1 };
-  }
-  console.log(obj);
-});
-// sline.forEach((item)=>{
+for (let i = 1; i < adata.length; i++) {
+  const wline = adata[i];
+  const awline = wline.split(",");
+  //*awline은 value값에 해당하는 배열 원본이다.
+  const larr = [];
+  larr.push(awline[0]);
+  //* larr은 awline의 0번 배열 모음이다, 한글
+  console.log(larr[1]);
+}
 
-// })
+function makeobj(a, b) {
+  let obj = {};
+  obj = { [a]: b };
+}
+// console.log(adata.length);
+// for (let i = 1; i < adata.length; i++) {
+//   const wline = adata[i];
+//   const swline = wline.split(",");
+//   // console.log(swline);
+//   for (let j = 0; j < sline.length; j++) {
+//     let result = objMake(sline[j], swline[j]);
+//     let stringresult = result.toString();
+//     console.log(result);
+//   }
+// }
+// function objMake(a, b) {
+//   let obj = {};
+//   obj[a] = b;
+//   return obj;
+// }
+// // console.log(sline[1]);
+// const nameline = swline[0];
+// console.log(nameline);
 // const wdata = adata[1];
 // const swdata = wdata.split(",");
 // // console.log(swdata);
