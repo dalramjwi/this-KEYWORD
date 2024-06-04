c43afdb7818a6df4b5559f33dd68e8002511b676
dalramjwi <dalramjwi@gmail.com>
2024-06-04 10:52:32 +0900

    같은 조건으로 b 또한 setter 적용

--- work-2.js
+++ work-2.js
@@ -10,6 +10,13 @@ class workTwo {
       throw new Error("a는 숫자만 입력 가능하다.");
     }
   }
+  set b(value) {
+    if (typeof value === "number") {
+      return (this._b = value);
+    } else {
+      throw new Error("b는 숫자만 입력 가능하다.");
+    }
+  }
 }
 let test = new workTwo("1", 2);
 console.log(test);
