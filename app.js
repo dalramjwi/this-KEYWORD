ebc5c502fb11fd1abca793171b6f6ea5438e264e
dalramjwi <dalramjwi@gmail.com>
2024-06-04 14:41:35 +0900

    readfile과 console.log의 순서 구분 위해 sync 를 사용함, 성공

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = "utf8.csv";
-const data = fs.readFiles(path, "utf-8", (err, data) => {
+const data = fs.readFileSync(path, "utf-8", (err, data) => {
   if (err) {
     console.error(err);
   }
